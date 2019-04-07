02942a018bd9642c9fba38be0a5f4e2d27f26995
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:01:16 +0545

    Initial commit

---
+++ app/src/main/java/com/e/a7th_april/MainActivity.java
@@ -0,0 +1,13 @@
+package com.e.a7th_april;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
